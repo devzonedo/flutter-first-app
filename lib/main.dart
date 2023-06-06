754d0c23f92e7ac6a748ada782d0819e73f7c038
Ravinath Fernando <ravinath_fernando@combank.net>
2023-06-06 18:15:44 +0530

    30. Generics, Lists & Adding Gradient Colors

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello YZRRR'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.deepPurple, Color.fromARGB(255, 53, 21, 109)],
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello YZRRR'),
+          ),
         ),
       ),
     ),
